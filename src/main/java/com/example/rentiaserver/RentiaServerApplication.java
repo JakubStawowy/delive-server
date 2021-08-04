2cf30299912e61ecc509e43d9e8eb0061cdcd2b9
Jakub <stawowykuba@gmail.com>
2021-08-04 19:24:20 +0200

    Initial commit

---
+++ src/main/java/com/example/rentiaserver/RentiaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.rentiaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentiaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentiaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rentiaserver/RentiaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rentiaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentiaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
